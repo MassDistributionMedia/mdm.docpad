70a753b2da300cc27376ac7114cdc5f6da21af46
Michael D. Mooring <mikeumus@gmail.com>
2016-02-17 03:09:16 +0000

    Fixing Edge Animation js file paths and removing cleanurls plugin so it doesnt clash with CloudFlare.

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -3,12 +3,8 @@ document.addEventListener("DOMContentLoaded", function(event){
 	var mainMenu = document.getElementById('main-menu');
 	
 	function onScrolledTop(){
-		// AdobeEdge.getComposition("docpad-keys-animation").getStage().play("start");
 		mainMenu.classList.add("main-menu-scrolled");
 	}
-	
-	// document.documentElement.className += " foo";
-	// document.documentElementstyle.borderBottom = '7px solid #005EAC';
 	
 	window.onscroll = function(){
 		document.documentElement.scrollTop || document.body.scrollTop > 13 ?
