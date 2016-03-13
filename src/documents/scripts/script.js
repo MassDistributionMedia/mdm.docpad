77ac5c24bfa324c1589cd8af87394d30b960a987
Michael D. Mooring <mikeumus@gmail.com>
2016-03-13 23:26:12 +0000

    Contact page/button, and SumoMe Email List builder plugin.

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -1,15 +1,17 @@
-document.addEventListener("DOMContentLoaded", function(event){
-
-	var mainMenu = document.getElementById('main-menu');
+(function(){
+	document.addEventListener("DOMContentLoaded", function(event){
 	
-	function onScrolledTop(){
-		mainMenu.classList.add("main-menu-scrolled");
-	}
+		var mainMenu = document.getElementById('main-menu');
+		
+		function onScrolledTop(){
+			mainMenu.classList.add("main-menu-scrolled");
+		}
+		
+		window.onscroll = function(){
+			document.documentElement.scrollTop || document.body.scrollTop > 13 ?
+				onScrolledTop()
+				: mainMenu.classList.remove("main-menu-scrolled");
+		};
 	
-	window.onscroll = function(){
-		document.documentElement.scrollTop || document.body.scrollTop > 13 ?
-			onScrolledTop()
-			: mainMenu.classList.remove("main-menu-scrolled");
-	};
-
-});
+	});
+})();
